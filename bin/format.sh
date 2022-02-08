5f2c1f48dc4943e5e9dabb9ad82561cb9c4f9c80
joshunrau <joshua@joshuaunrau.com>
2022-02-08 17:26:29 -0500

    update format.sh

--- bin/format.sh
+++ bin/format.sh
@@ -3,7 +3,7 @@
 declare -a sourceDirs=("app" "lib")
 
 for dir in "${sourceDirs[@]}"; do
-  find "./${dir}" -name '*.css' | xargs ./node_modules/.bin/css-beautify -r
-  find "./${dir}" -name '*.html' | xargs ./node_modules/.bin/html-beautify --r
-  find "./${dir}" -name '*.js' | xargs ./node_modules/.bin/js-beautify -r
+  find "./${dir}" -name '*.css' | xargs css-beautify -r
+  find "./${dir}" -name '*.html' | xargs html-beautify -r
+  find "./${dir}" -name '*.js' | xargs js-beautify -r
 done
