26d4c065a3bb79c6a3c79e70d9384305973e4d88
joshunrau <joshua@joshuaunrau.com>
2022-02-08 16:38:14 -0500

    divide app

--- app/js/popup.js
+++ app/js/popup.js
@@ -1,6 +1,6 @@
 const button = document.getElementById("download-button");
 const inputUrl = document.getElementById("input-url");
-const test = document.getElementById("test");
+
 
 button.addEventListener('click', () => {
     test.innerText = "You pressed the button";
