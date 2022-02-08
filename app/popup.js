5b894725371b2f391fb1d099ec0a340107111144
joshunrau <joshua@joshuaunrau.com>
2022-02-08 13:13:00 -0500

    add ext test

---
+++ app/popup.js
@@ -0,0 +1,9 @@
+const button = document.getElementById("download-button");
+const inputUrl = document.getElementById("input-url");
+const test = document.getElementById("test");
+
+button.addEventListener('click', () => {
+    test.innerText = "You pressed the button";
+});
+
+
