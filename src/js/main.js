ef0042d86c2fe1a6860e558e0e62e1a647fef1a3
joshunrau <joshua@joshuaunrau.com>
2022-02-10 18:29:11 -0500

    organize repo

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,10 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
-const cp = require("child_process");
-const ffmpeg = require('ffmpeg-static');
-const fs = require('fs');
-const id3 = require('node-id3');
 const path = require('path');
-const stream = require('stream');
-const ytdl = require('ytdl-core');
 
 const appPath = app.getAppPath();
 console.log("Registered application path: " + appPath);
 
-const downloadsDirectory = path.join(process.env.HOME || process.env.USERPROFILE, 'Downloads');
-console.log("Registered downloads directory: " + downloadsDirectory);
+const Song = require(path.join(appPath, 'src', 'js', 'song.js'));
 
 function createWindow() {
     const mainWindow = new BrowserWindow({
@@ -39,85 +32,3 @@ ipcMain.on("download-triggered", (_event, url) => {
         console.error("ERROR: " + error.message);
     });
 });
-
-/*****************************************************/
-
-function pathExists(myPath) {
-    try {
-        fs.accessSync(myPath, fs.constants.F_OK);
-        return true;
-    } catch {
-        return false;
-    };
-};
-
-class Song {
-
-    // change based on info later
-    audioFile = path.join(downloadsDirectory, "audio.mp3");
-    videoFile = path.join(downloadsDirectory, "video.mp4");
-
-    ffmpegCommand = `${ffmpeg} -loglevel 24 -i ${this.videoFile} ${this.audioFile}`;
-
-    constructor(url) {
-        this.url = url;
-        // this.title = info.videoDetails.title;
-    };
-
-    async getInfo() {
-        return ytdl.getInfo(this.url, {
-                quality: 'highestaudio'
-            })
-            .then(info => {
-                this.info = info;
-            })
-            .catch(error => {
-                return error;
-            });
-    };
-
-    async downloadVideo() {
-
-        if (pathExists(this.videoFile)) {
-            return new Error("ERROR: Video file already exists!");
-        };
-
-        return stream.promises.pipeline(
-            ytdl.downloadFromInfo(this.info),
-            fs.createWriteStream(this.videoFile)
-        );
-    };
-
-    // Convert mp4 video file into mp3 using ffmpeg
-    convertVideoToAudio() {
-
-        if (pathExists(this.audioFile)) {
-            return new Error("ERROR: Audio file already exists!");
-        };
-
-        console.log("Verified that audio file does not already exist!");
-
-        cp.execSync(this.ffmpegCommand);
-        console.log("File conversion successful!"); // try to implement as callback
-
-        fs.rmSync(this.videoFile);
-        console.log("Successfully removed video file!"); // try to implement as callback
-
-    };
-
-    clearMetaData() {
-        id3.removeTags(this.audioFile, (error) => {
-            if (error) {
-                return error;
-            } else {
-                console.log("Metadata removal successful!")
-            };
-        });
-    };
-
-    addMetaData(info) {
-        let title = info.videoDetails.title;
-        console.log(title);
-    };
-
-};
