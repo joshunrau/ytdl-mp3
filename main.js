bd188b794fbd9fff7c50da7300a10b229ed78de5
joshunrau <joshua@joshuaunrau.com>
2022-02-10 17:45:45 -0500

    working to same level as command line

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
-const { info } = require('console');
 const { app, BrowserWindow, ipcMain } = require('electron');
+const cp = require("child_process");
+const ffmpeg = require('ffmpeg-static');
 const fs = require('fs');
+const id3 = require('node-id3');
 const path = require('path');
 const stream = require('stream');
 const ytdl = require('ytdl-core');
 
-// Set filepaths
 const downloadsDirectory = path.join(process.env.HOME || process.env.USERPROFILE, 'Downloads');
-const videoFile = path.join(downloadsDirectory, "video.mp4");
-const audioFile = path.join(downloadsDirectory, "audio.mp3");
 console.log("Registered downloads directory: " + downloadsDirectory);
 
 function createWindow() {
@@ -17,9 +16,8 @@ function createWindow() {
         height: 600,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
-          }
+        }
     });
-    // mainWindow.webContents.openDevTools();
     mainWindow.loadFile('index.html');
 };
 
@@ -29,100 +27,82 @@ app.whenReady().then(() => {
 
 ipcMain.on("download-triggered", (_event, url) => {
     console.log("Revieved URL: " + url);
-    let videoInfo = downloadVideo(url, videoFile);
-    videoInfo.then(() => {
-        console.log("Done!");
-    })
+    let song = new Song(url);
+    song.getInfo().then(() =>
+        song.downloadVideo()
+    ).then(() => {
+        song.convertVideoToAudio();
+    }).catch(error => {
+        console.error("ERROR: " + error.message);
+    });
 });
 
-// Download video and return info from YouTube
-async function downloadVideo(url, outputFile) {
-    let videoInfo = await ytdl.getInfo(url);
-    try {
-        fs.accessSync(outputFile, fs.constants.F_OK);
-        console.log("File Exists!");
-        return info;
-    } catch (error) {
-        console.log("File Does Not Exist");
+/*****************************************************/
+
+class Song {
+
+    // change based on info later
+    audioFile = path.join(downloadsDirectory, "audio.mp3");
+    videoFile = path.join(downloadsDirectory, "video.mp4");
+
+    ffmpegCommand = `${ffmpeg} -loglevel 24 -i ${this.videoFile} ${this.audioFile}`;
+
+    constructor(url) {
+        this.url = url;
+        // this.title = info.videoDetails.title;
     };
-    let streamOptions = {autoClose: true, flags: 'w'};
-    let writeStream = fs.createWriteStream(outputFile, streamOptions);
-    console.log("Attempting download of " + videoInfo.videoDetails.title)
-    let readStream = ytdl.downloadFromInfo(videoInfo, {
-        quality: 'highestaudio'
-    });
-    await stream.promises.pipeline(readStream, writeStream)
-};
 
-/*
-
-const cp = require("child_process");
-const ffmpeg = require('ffmpeg-static');
-const fs = require('fs');
-const id3 = require('node-id3');
-
-
-
-
-// Download video and return info from YouTube
-async function downloadVideo(url, outputFile) {
-    let info = await ytdl.getInfo(url);
-    let streamOptions = {
-        autoClose: true,
-        flags: 'w'
+    async getInfo() {
+        return ytdl.getInfo(this.url, {
+                quality: 'highestaudio'
+            })
+            .then(info => {
+                this.info = info;
+            })
+            .catch(error => {
+                return error;
+            });
     };
-    try {
-        fs.accessSync(outputFile, fs.constants.F_OK)
-    } catch (error) {
-        return info
-    }
-    let writeStream = fs.createWriteStream(outputFile, streamOptions);
-    let readStream = ytdl.downloadFromInfo(info, {
-        quality: 'highestaudio'
-    });
-    return (async () => {
-        await stream.promises.pipeline(readStream, writeStream);
-        return info
-    })()
+
+    async downloadVideo() {
+        return stream.promises.pipeline(
+            ytdl.downloadFromInfo(this.info),
+            fs.createWriteStream(this.videoFile)
+        );
+    };
+
+    // Convert mp4 video file into mp3 using ffmpeg
+    convertVideoToAudio() {
+
+        try {
+            fs.accessSync(this.audioFile, fs.constants.F_OK);
+            console.error("ERROR: Audio file already exists!");
+            return null; // return error once I learn how to catch them properly
+        } catch (error) {
+            console.log("Verified that audio file does not already exist!");
+        };
+
+        cp.execSync(this.ffmpegCommand);
+        console.log("File conversion successful!"); // try to implement as callback
+
+        fs.rmSync(this.videoFile);
+        console.log("Successfully removed video file!"); // try to implement as callback
+
+    };
+
+    clearMetaData() {
+        id3.removeTags(this.audioFile, (error) => {
+            if (error) {
+                return error;
+            } else {
+                console.log("Metadata removal successful!")
+            };
+        });
+    };
+
+    addMetaData(info) {
+        let title = info.videoDetails.title;
+        console.log(title);
+    };
+
 };
-
-// Convert mp4 video file into mp3 using ffmpeg
-function convertVideoToAudio(inputFile, outputFile) {
-    let cmd = `${ffmpeg} -loglevel 24 -i ${inputFile} ${outputFile}`;
-    try {
-        fs.accessSync(outputFile, fs.constants.F_OK)
-    } catch (error) {
-        return
-    }
-    cp.execSync(cmd, (error, stdout, stderr) => {
-        if (error) console.log(`error: ${error.message}`);
-        else if (stderr || stdout) console.log(`File conversion successful!`);
-    });
-    // fs.rmSync(inputFile);
-};
-
-function clearMetaData(audioFile) {
-    id3.removeTags(audioFile, (err) => {
-        if (err) {
-            throw new Error("Metadata removal unsuccessful!")
-        } else {
-            console.log("Metadata removal successful!")
-        }
-    });
-}
-
-function addMetaData(info) {
-    let title = info.videoDetails.title;
-    console.log(title);
-}
-
-async function main() {
-    let videoInfo = await downloadVideo(url, videoFile);
-    // console.log(Object.keys(videoInfo))
-    // convertVideoToAudio(videoFile, audioFile);
-    // clearMetaData(audioFile);
-}
-
-main();
-
-*/
