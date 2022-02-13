309c73c01c638c5457dcca4697ec9569444f9086
joshunrau <joshua@joshuaunrau.com>
2022-02-13 00:35:38 -0500

    add command line

--- README.md
+++ README.md
@@ -2,4 +2,3 @@
 
 ## About
 This project allows you to easily download YouTube videos, convert them to mp3 format, and scrape the web for relevant cover art and metadata.
-
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 #!/usr/bin/env node
 
+const { program } = require('commander');
+const pkg = require('../package.json');
 const Song = require("./song");
 const ytdl = require('ytdl-core');
 
-const sampleURL = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+program.name(pkg.name);
+program.description(pkg.description);
+program.version(pkg.version);
+program.allowExcessArguments(false);
+program.argument("<url>", "url of video to download");
+program.parse();
 
 const main = async () => {
-    song = await ytdl.getInfo(sampleURL, {quality: 'highestaudio'})
+    song = await ytdl.getInfo(program.args[0], {quality: 'highestaudio'})
         .then(info => new Song(info))
         .then(song => song.downloadVideo())
         .then(song => song.convertVideoToAudio())
