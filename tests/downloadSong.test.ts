7d1fad2abe2e6db01c71dc3f8b9bf9d5fe1a6a5d
unknown <kinho.sjdr@gmail.com>
2023-01-24 21:30:36 -0300

    redone tests - implementation test with new return of function downloadSong

--- tests/downloadSong.test.ts
+++ tests/downloadSong.test.ts
@@ -4,14 +4,26 @@ import { getDownloadsDir } from '../src/utils';
 
 const url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
 
-test('downloadSong', async () => {
-  await expect(
-    downloadSong(url, { outputDir: 'probably/not/a/directory' })
-  ).rejects.toThrowError(NotADirectoryError);
-  await expect(
-    downloadSong('https://www.google.com', { outputDir: getDownloadsDir() })
-  ).rejects.toThrowError(VideoInfoFetchError);
-  await expect(
-    downloadSong(url, { outputDir: getDownloadsDir() })
-  ).resolves.not.toThrowError();
-}, 120000);
+describe('Test of function downloadSong', () => {
+
+  test('It should return an error. the directory no exists!', async () => {
+
+    await expect(downloadSong(url, { outputDir: 'probably/not/a/directory' })
+    ).rejects.toThrowError(NotADirectoryError);
+
+  });
+
+  test('It should return an error. The URL no is valide or no exists', async () => {
+    await expect(
+      downloadSong('https://www.google.com', { outputDir: getDownloadsDir() })
+    ).rejects.toThrowError(VideoInfoFetchError);
+  });
+
+  test('It should download of file audio and return name file of audio ', async () => {
+    await expect(
+      downloadSong(url, { outputDir: getDownloadsDir() })
+    ).resolves.not.toBeUndefined();
+  }, 12000);
+
+
+});
