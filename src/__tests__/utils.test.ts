50750cf56b6cc5e3a075abe68e8608ffd30e399f
joshunrau <contact@joshuaunrau.com>
2024-06-18 19:19:44 -0400

    test: add test for userInput

--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -1,6 +1,9 @@
-import { describe, expect, it } from 'vitest';
+import readline from 'readline';
 
-import { isDirectory, removeParenthesizedText } from '../utils';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
+import type { MockedFunction } from 'vitest';
+
+import { YtdlMp3Error, isDirectory, removeParenthesizedText, userInput } from '../utils';
 
 describe('removeParenthesizedText', () => {
   it('removes content within square brackets', () => {
@@ -39,5 +42,28 @@ describe('isDirectory', () => {
   });
   it('should return true for a non-existent file', () => {
     expect(isDirectory('/dev/null/foo.js')).toBe(false);
+  });
+});
+
+describe('userInput', () => {
+  let question: MockedFunction<(query: string, callback: (answer: string) => void) => void>;
+
+  beforeEach(() => {
+    question = vi.fn();
+    vi.spyOn(readline, 'createInterface').mockReturnValueOnce({
+      close: vi.fn(),
+      question,
+      write: vi.fn()
+    } as any);
+  });
+
+  it('should return the response to the question, if it is a non-empty string', async () => {
+    question.mockImplementationOnce((_, callback) => callback('hello'));
+    const response = await userInput('Please enter a response: ');
+    expect(response).toBe('hello');
+  });
+  it('should throw an error if the response is an empty string', async () => {
+    question.mockImplementationOnce((_, callback) => callback(''));
+    await expect(() => userInput('Please enter a response: ')).rejects.toBeInstanceOf(YtdlMp3Error);
   });
 });
