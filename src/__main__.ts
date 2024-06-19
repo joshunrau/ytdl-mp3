18ddb03a45e9b86c4d7554b5326561dae2429102
joshunrau <contact@joshuaunrau.com>
2024-06-18 20:26:53 -0400

    test: add Downloader tests

--- src/__main__.ts
+++
@@ -1,6 +0,0 @@
-import { removeParenthesizedText } from './utils';
-
-const result = removeParenthesizedText('()');
-console.log(result);
-
-console.log(import.meta.filename);
--- src/__tests__/Downloader.test.ts
+++ src/__tests__/Downloader.test.ts
@@ -1,10 +1,13 @@
 import fs from 'fs';
 import path from 'path';
+import { PassThrough } from 'stream';
 
 import { afterAll, beforeAll, beforeEach, describe, expect, it, vi } from 'vitest';
+import type { Mock } from 'vitest';
 import ytdl from 'ytdl-core';
 
 import { Downloader } from '../Downloader';
+import { FormatConverter } from '../FormatConverter';
 import * as utils from '../utils';
 
 const { YtdlMp3Error } = utils;
@@ -23,26 +26,36 @@ describe('Downloader', () => {
 
   describe('downloadSong', () => {
     let downloader: Downloader;
+    let downloadStream: PassThrough;
+    let formatConverter: { videoToAudio: Mock };
 
     beforeEach(() => {
-      downloader = new Downloader({
-        getTags: false,
-        outputDir
-      });
+      downloader = new Downloader({ getTags: false, outputDir });
+      downloadStream = new PassThrough();
+      formatConverter = { videoToAudio: vi.fn() };
+      vi.spyOn(ytdl, 'getInfo').mockResolvedValue({ videoDetails: { title: '' } } as any);
+      vi.spyOn(ytdl, 'downloadFromInfo').mockReturnValue(downloadStream as any);
+      vi.spyOn(FormatConverter.prototype, 'videoToAudio').mockImplementation(formatConverter.videoToAudio);
     });
 
     it('should throw if the output directory does not exist', async () => {
       vi.spyOn(utils, 'isDirectory').mockReturnValueOnce(false);
       await expect(downloader.downloadSong(url)).rejects.toBeInstanceOf(YtdlMp3Error);
     });
-    // it('should call ytdl.getInfo', async () => {
-    //   vi.spyOn(ytdl, 'getInfo').mockResolvedValueOnce({} as any);
-    //   await downloader.downloadSong(url);
-    //   expect(ytdl.getInfo).toHaveBeenCalledOnce();
-    // });
-    // it('should throw a YtdlMp3Error if ytdl.getInfo rejects', async () => {
-    //   vi.spyOn(ytdl, 'getInfo').mockRejectedValueOnce(new Error());
-    //   await expect(() => downloader.downloadSong(url)).rejects.toBeInstanceOf(YtdlMp3Error);
-    // });
+    it('should call ytdl.getInfo', async () => {
+      const promise = downloader.downloadSong(url);
+      expect(ytdl.getInfo).toHaveBeenCalledOnce();
+      downloadStream.end();
+      await promise;
+    });
+    it('should throw a YtdlMp3Error if ytdl.getInfo rejects', async () => {
+      vi.spyOn(ytdl, 'getInfo').mockRejectedValueOnce(new Error());
+      await expect(() => downloader.downloadSong(url)).rejects.toBeInstanceOf(YtdlMp3Error);
+    });
+    it('should return the output file', async () => {
+      const promise = downloader.downloadSong(url);
+      downloadStream.end();
+      await expect(promise).resolves.toBeTypeOf('string');
+    });
   });
 });
