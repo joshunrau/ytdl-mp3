fa6144c625f6e8d28f4e493fc55708000e5fb79c
joshunrau <contact@joshuaunrau.com>
2024-08-25 12:14:41 -0400

    refactor!: move main into bin
    BREAKING CHANGE: the main export has been removed since the version was incorrect

--- bin/ytdl-mp3.cjs
+++ bin/ytdl-mp3.cjs
@@ -1,5 +1,37 @@
 #!/usr/bin/env node
 
-const { main } = require('../dist/index.cjs');
+const { Command } = require('commander');
 
-main();
+const { description, name, version } = require('../package.json');
+const { Downloader, YtdlMp3Error } = require('../dist/index.cjs');
+
+const program = new Command();
+program.name(name);
+program.description(description);
+program.version(version);
+program.allowExcessArguments(false);
+program.argument('<url>', 'url of video to download');
+program.option('-o --output-dir <path>', 'path to output directory', Downloader.defaultDownloadsDir);
+program.option('-n --no-get-tags', 'skip extracting/applying id3 tags');
+program.option('-v --verify-tags', 'verify id3 tags fetched from itunes');
+program.option('-s --silent-mode', 'skip console output');
+program.option('--verbose', 'enable verbose mode');
+program.parse();
+
+(async function () {
+  const options = program.opts();
+  try {
+    const downloader = new Downloader(options);
+    await downloader.downloadSong(program.args[0]);
+  } catch (err) {
+    if (err instanceof YtdlMp3Error) {
+      if (options.verbose) {
+        console.error(err.cause);
+        console.error(err.stack);
+      }
+      console.error(`ERROR: ${err.message}`);
+      process.exit(1);
+    }
+    throw err;
+  }
+})();
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export * from './Downloader';
 export * from './FormatConverter';
 export * from './SongTagsSearch';
-export { main } from './main';
 export { YtdlMp3Error } from './utils';
